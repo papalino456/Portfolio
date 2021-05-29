6f033ddb6a5c69af051c8f9a5d14d2bdea465bff
papalino456 <sebastian.barrio.b@gmail.com>
2021-05-29 18:55:44 -0500

    Express server

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+let express = require('express');
+let app = express();
+let port = 3000;
 
 app.use(express.static('views'));
 
-app.get('/', (req, res) => {
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at port ${port}`);
+});
+
+app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/main.html")
 });
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
