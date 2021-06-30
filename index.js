8f1ff9a822b57856906a5d7794e1161655ac8f53
papalino456 <sebastian.barrio.b@gmail.com>
2021-06-30 15:50:30 -0500

    add personal automatation endpoints

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let express = require('express');
+var bodyParser = require('body-parser');
 const path = require("path")
+
 let app = express();
 let port = 3000;
+var data;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "frontend/out")));
 
 app.listen(process.env.PORT || port, () => {
@@ -13,3 +17,11 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "frontend/wip.html"))
 });
 
+app.post("/button", function(req,res){
+  data = req.body;
+  res.end
+});
+
+app.get("/button", function(req,res){
+  res.send(data);
+});
